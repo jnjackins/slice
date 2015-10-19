329b1b2d05d002ee676ea485bb9624b349fa8547
Jeremy Jackins <jeremyjackins@gmail.com>
2015-10-19 11:43:16 +0900

    update tests

--- slice_test.go
+++ slice_test.go
@@ -6,21 +6,34 @@ import (
 )
 
 func TestSlice(t *testing.T) {
+	t.Log("opening stl file")
 	f, err := os.Open("./testdata/pikachu.stl")
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	t.Log("parsing stl")
 	stl, err := Parse(f)
 	if err != nil {
 		t.Fatal(err)
 	}
+	f.Close()
 
 	var cfg = Config{
-		LayerHeight: 1.0,
+		LayerHeight:   1.0,
+		LineWidth:     1.0,
+		InfillSpacing: 2.0,
+		InfillAngle:   45.0,
 	}
 
-	err = stl.Slice(os.Stdout, cfg)
+	t.Log("slicing with output=./testdata/output.gcode")
+	out, err := os.Create("./testdata/output.gcode")
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
+	err = stl.Slice(out, cfg)
+	if err != nil {
+		t.Fatal(err)
+	}
+	out.Close()
 }
