6182e977206aca4d3891d25de7565695473ee272
Jeremy Jackins <jeremyjackins@gmail.com>
2015-11-23 13:17:59 +0900

    remove extra args to fprintf

--- gcode.go
+++ gcode.go
@@ -11,13 +11,13 @@ func (l *Layer) Gcode() []byte {
 	for _, solid := range l.solids {
 		//perimeters
 		s := solid.exterior[0]
-		fmt.Fprintf(buf, "G1 X%.5f Y%.5f\n", s.from.X, s.from.Y, 0.0)
+		fmt.Fprintf(buf, "G1 X%.5f Y%.5f\n", s.from.X, s.from.Y)
 		for _, s := range solid.exterior {
 			fmt.Fprintf(buf, "G1 X%.5f Y%.5f E%.5f\n", s.to.X, s.to.Y, 0.0)
 		}
 		for _, p := range solid.interiors {
 			s := p[0]
-			fmt.Fprintf(buf, "G1 X%.5f Y%.5f\n", s.from.X, s.from.Y, 0.0)
+			fmt.Fprintf(buf, "G1 X%.5f Y%.5f\n", s.from.X, s.from.Y)
 			for _, s := range p {
 				fmt.Fprintf(buf, "G1 X%.5f Y%.5f E%.5f\n", s.to.X, s.to.Y, 0.0)
 			}
